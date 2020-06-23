3acac7324017bba0e9240c24b718be7db024d796
smsqadri <smsqadri@gmail.com>
2020-06-24 02:03:05 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export const Header =() => {
   return (
     <div className="bgWhite">
       <h1>
-        Expense <span>Manager</span> <br />
+        Expensulator <span></span> <br />
         <a href="http://github.com/abufurqan/">by Shabbir Qadri</a>
       </h1>
     </div>
